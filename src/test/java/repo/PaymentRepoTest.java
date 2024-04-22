f6593e6850913c11f3d0921b126a41fc4cfa8f98
Ovidiu Barac <ovidiu.barac@pitech.webhelp.com>
2024-04-22 15:07:22 +0300

    tests

---
+++ src/test/java/repo/PaymentRepoTest.java
@@ -0,0 +1,36 @@
+package repo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+import pizzashop.model.Payment;
+import pizzashop.model.PaymentType;
+import pizzashop.repository.PaymentRepository;
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+class PaymentRepoTest {
+  @Spy
+  PaymentRepository repo;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  void getPaymentTest() {
+    assertEquals(repo.getPayment("4,CASH,13.97").getAmount(), new Payment(4, PaymentType.CASH, 13.97).getAmount());
+    assertEquals(repo.getPayment("4,CASH,13.97").getTableNumber(), new Payment(4, PaymentType.CASH, 13.97).getTableNumber());
+    assertEquals(repo.getPayment("4,CASH,13.97").getType(), new Payment(4, PaymentType.CASH, 13.97).getType());
+  }
+
+  @Test
+  void readPaymentsTest(){
+    when(repo.getPayment(anyString())).thenReturn(new Payment(4, PaymentType.CASH, 13.97));
+    repo.readPayments();
+    verify(repo, times(59)).getPayment(anyString());
+  }
+}
